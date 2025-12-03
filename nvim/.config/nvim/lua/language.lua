d1d1dbc9c9de468d79d483b639afc387156073df
skdishansachin <skdishansachin@gmail.com>
2025-12-03 08:48:08 +0530

    add lua language style formatting

--- nvim/.config/nvim/lua/language.lua
+++ nvim/.config/nvim/lua/language.lua
@@ -1,8 +1,20 @@
+-- go
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "go",
   callback = function()
     vim.opt_local.expandtab = false
     vim.opt_local.shiftwidth = 4
     vim.opt_local.tabstop = 4
+  end,
+})
+
+-- lua
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "lua",
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
   end,
 })
