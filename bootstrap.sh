b428b832f686527d17c18d2746a2083dfd560d55
skdishansachin <skdishansachin@gmail.com>
2025-12-04 20:47:40 +0530

    add the playerctl program to install

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ sudo apt -y upgrade
 
 echo "[*] Installing packages…"
 sudo apt install -y \
-    git curl tmux jq stow neovim zoxide fzf ripgrep starship poppler-utils
+    git curl tmux jq stow neovim zoxide fzf ripgrep starship poppler-utils playerctl
 
 echo "[*] Preparing fonts directory…"
 mkdir -p "$HOME/Downloads"
