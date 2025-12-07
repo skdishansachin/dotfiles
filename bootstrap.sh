d096f02af62e51d3b5d6eff4907e8a4fede61505
skdishansachin <skdishansachin@gmail.com>
2025-12-07 14:45:16 +0530

    remove the jetbrains mono font from installing

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ sudo apt -y upgrade
 
 echo "[*] Installing base packages…"
 sudo apt install -y \
-    git curl tmux jq stow alacritty neovim zoxide fzf ripgrep poppler-utils fonts-jetbrains-mono
+    git curl tmux jq stow alacritty neovim zoxide fzf ripgrep poppler-utils
 
 
 echo
