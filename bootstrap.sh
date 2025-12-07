d495c61f9709f36d3b072ff8f30950f3987fe9b7
skdishansachin <skdishansachin@gmail.com>
2025-12-07 14:46:47 +0530

    update the installation script

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,29 +7,8 @@ sudo apt -y upgrade
 
 echo "[*] Installing base packages…"
 sudo apt install -y \
-    git curl tmux jq stow alacritty neovim zoxide fzf ripgrep poppler-utils
-
-
-echo
-echo "[*] Desktop Environment Selection"
-echo "1) Setup i3"
-echo "2) Setup KDE"
-read -rp "Choose your setup [1/2]: " choice
-
-case "$choice" in
-    1)
-        echo "[*] Installing i3 packages…"
-        sudo apt install -y dmenu feh playerctl brightnessctl
-        ;;
-    2)
-        echo "[*] Installing KDE packages…"
-	echo "NOT YET IMPLEMENTED!"
-	;;
-    *)
-        echo "Invalid choice. Exiting."
-        exit 1
-        ;;
-esac
+    git curl tmux jq stow alacritty neovim zoxide fzf ripgrep poppler-utils \
+    dmenu feh playerctl brightnessctl
 
 echo "[*] Coping the wallpaper"
 mkdir -p "$HOME/Pictures/wallpapers/"
