e4730f56dbb2bc93e829ab577477971133658651
skdishansachin <skdishansachin@gmail.com>
2025-12-09 20:01:50 +0530

    add the C lang style

--- nvim/.config/nvim/lua/language.lua
+++ nvim/.config/nvim/lua/language.lua
@@ -18,3 +18,14 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.softtabstop = 2
   end,
 })
+
+-- C
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "c",
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+  end,
+})
