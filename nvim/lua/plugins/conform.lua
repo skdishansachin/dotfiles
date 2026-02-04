43309d8b867f21899d4b03c8ea3f069419b8b138
skdishansachin <134765302+skdishansachin@users.noreply.github.com>
2026-02-04 12:24:12 +0530

    remove comments and language sepcific formatting

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -6,38 +6,11 @@ return {
     local conform = require("conform")
 
     conform.setup({
-      formatters_by_ft = {
-        javascript = function(bufnr)
-          -- Check if Bun lockfile exists
-          local is_bun = vim.fs.find({ "bun.lockb", "bun.lock" }, {
-            upward = true,
-            path = vim.api.nvim_buf_get_name(bufnr)
-          })[1]
+      formatters_by_ft = {},
 
-          if is_bun then
-            return { "bun" }
-          end
-
-          -- Fallback for Node/Standard projects
-          return { "prettierd", "prettier", stop_after_first = true }
-        end,
-
-        typescript = function(bufnr)
-          -- Same logic for TS
-          local is_bun = vim.fs.find({ "bun.lockb", "bun.lock" }, {
-            upward = true,
-            path = vim.api.nvim_buf_get_name(bufnr)
-          })[1]
-
-          if is_bun then return { "bun" } end
-          return { "prettierd", "prettier", stop_after_first = true }
-        end,
-      },
-
-      -- Crucial: This handles the "Save" behavior
       format_on_save = {
         timeout_ms = 500,
-        lsp_format = "fallback", -- Use LSP only if Bun/Prettier aren't found
+        lsp_format = "fallback",
       },
     })
   end,
