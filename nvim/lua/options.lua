bc8877216c5f3eb026da1d3d0b533de12f88c6dd
skdishansachin <134765302+skdishansachin@users.noreply.github.com>
2026-02-16 22:58:22 +0530

    removed unused vim options

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,46 +1,21 @@
--- Basic settings
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.showmode = false
-vim.o.mouse = "a"
+vim.o.cursorline = true
+vim.o.scrolloff = 10
+vim.o.signcolumn = "yes"
 
--- Clipboard (set asynchronously)
-vim.schedule(function()
-  vim.o.clipboard = "unnamedplus"
-end)
-
--- Editing behavior
 vim.o.breakindent = true
 vim.o.undofile = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
-vim.o.signcolumn = "yes"
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
 vim.o.splitright = true
 vim.o.splitbelow = true
-vim.o.inccommand = "split"
-vim.o.cursorline = true
-vim.o.scrolloff = 10
 vim.o.confirm = true
+vim.o.mouse = "a"
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+vim.o.inccommand = "split"
 
--- Display options
 vim.o.list = true
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
-
--- Fold
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-vim.opt.foldtext = ""
-vim.opt.foldlevel = 99
-vim.opt.foldlevelstart = 1
-vim.opt.foldnestmax = 1
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function()
-    vim.lsp.buf.format({ async = false })
-  end,
-})
-
--- vim: ts=2 sts=2 sw=2 et
