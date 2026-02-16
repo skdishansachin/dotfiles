48e2309870a3cc0ba09fc5b7f518a900dfad3da5
skdishansachin <134765302+skdishansachin@users.noreply.github.com>
2026-02-16 21:09:13 +0530

    remove comments plugin

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -3,7 +3,6 @@ require("lazy").setup({
   require("plugins.telescope"),
   require("plugins.treesitter"),
   require("plugins.neo-tree"),
-  require("plugins.comments"),
 }, {
   ui = {
     icons = vim.g.have_nerd_font and {},
