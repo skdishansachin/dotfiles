5cabb20a01caf5b377bce5a1d3d7cede47d3aafc
skdishansachin <134765302+skdishansachin@users.noreply.github.com>
2026-02-16 20:55:13 +0530

    enable language servers, configuration for diagnostics and custom keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -19,5 +19,49 @@ require("lazy-plugins")
 
 vim.cmd("colorscheme tokyonight-night")
 
+vim.lsp.enable({ "lua_ls", "ty", "rust_analyzer" })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+  callback = function(event)
+    local map = function(keys, func, desc, mode)
+      mode = mode or "n"
+      vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+    end
+
+    map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
+    map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
+    map("grr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+    map("gri", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+    map("grd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+    map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+    map("gO", require("telescope.builtin").lsp_document_symbols, "Open Document Symbols")
+    map("gW", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Open Workspace Symbols")
+    map("grt", require("telescope.builtin").lsp_type_definitions, "[G]oto [T]ype Definition")
+    map("<leader>e", function() vim.diagnostic.open_float(nil, { focus = false }) end, "Show Line Diagnostics")
+    map("[e", function() vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR }) end,
+      "Previous Error")
+    map("]e", function() vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR }) end, "Next Error")
+    map("<leader>th", function()
+      vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+    end, "[T]oggle Inlay [H]ints")
+  end,
+})
+
+-- diagnostic config
+vim.diagnostic.config({
+  severity_sort = true,
+  float = { border = "rounded", source = "if_many", max_width = 80, wrap = true },
+  underline = { severity = vim.diagnostic.severity.ERROR },
+  signs = {},
+  virtual_text = {
+    source = "if_many",
+    spacing = 2,
+    format = function(diagnostic)
+      return diagnostic.message
+    end,
+  },
+})
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
